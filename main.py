845c9417f12256b4647201a622eee8e6b663434b
imcoref <music252@gmx.de>
2026-02-11 11:03:54 +0100

    wxData in different file

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ from retry_requests import retry
 import folium
 from streamlit_folium import st_folium
 
+from data.weatherData_decoded import wmoData
+
 
 st.set_page_config(page_title="AZT History Weather", page_icon="desert" ,layout="wide")
 st.title("🏔 AZT  History Weather")
@@ -58,18 +60,7 @@ longitudeNOBO = [-110.2827364,-110.3679495,-110.5569114,-110.7253987,-110.730044
 milemarkerNOBO = [0,13,34,52,75,111,119,135,141,150,164,184,205,263,300,344,400,455,496,535,550,\
 					597,632,694,703,711,718,750,767,795]
 
-# Wetter-Codes
-wmoData = { 0.0: "Clear", 1.0: "Partly Cloudy", 2.0: "Mostly Clear", \
-			3.0: "Cloudy", 45.0: "Fog", 48.0: "Freezing Fog", \
-			51.0: "Light Drizzle", 53.0: "Drizzle", 55.0: "Heavy Drizzle", \
-			56.0: "Light Freezing Drizzle", 57.0: "Freezing Drizzle", 
-			61.0: "Light Rain", 63.0: "Rain", 65.0: "Heavy Rain", \
-			66.0: "Light Freezing Rain", 67.0: "Freezing Rain", \
-			71.0: "Light Snow", 73.0: "Snow", 75.0: "Heavy Snow", \
-			77.0: "Snow Grains", 80.0: "Light Rain Shower", \
-			81.0: "Rain Shower", 82.0: "Heavy Rain Shower", \
-			85.0: "Snow Shower", 86.0: "Heavy Snow Shower", \
-			95.0: "Thunderstorm", 96.0: "Hailstorm", 99.0: "Heavy Hailstorm"}
+
 
 # SOBO Listen
 locationsSOBO = list(reversed(locationsNOBO))
