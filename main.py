86a4d621711d756490d4f1573f70f74980ec9116
imcoref <93718664+imcoref@users.noreply.github.com>
2026-02-28 12:35:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -45,7 +45,8 @@ def main():
     # -------------------------------------------------
 
     #trail_name = sys.argv[1]
-    trail_name = st.secrets["TRAIL_NAME"]
+    #trail_name = st.secrets["TRAIL_NAME"]
+    trail_name = 'AZT'
     emblem_image = 'data/' + trail_name + '_emblem.png'
     if not os.path.isfile(emblem_image):
         emblem_image = False
